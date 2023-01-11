6576f7fa5e93f073ab5c9bfa8a4dbaafee347dad
Maximilian Karlander <mkarla@kth.se>
2023-01-11 16:03:46 +0100

    added options of number of residues to show in pymol

--- code/capyflex_epiplot.py
+++ code/capyflex_epiplot.py
@@ -22,7 +22,14 @@ import seaborn as sns
 import numpy as np
 np.seterr(all="ignore")
 
-def make_filelist(input_dir):
+
+"""
+- options for number of top positions
+    - write all positions commented?
+    - add commented value for each position?
+"""
+
+def make_filelist(input_dir, args):
     if input_dir[-1] == '/':
         input_dir = input_dir
     else:
@@ -36,7 +43,12 @@ def make_filelist(input_dir):
     if len(files) == 0:
         print('No results files in input folder.')
         sys.exit(1)
-    return(files)
+    
+    if args.Custom:
+        files, tops = custom_files(args.Custom, files)
+    else:
+        tops=[args.Top]*len(files)
+    return(files, tops)
 
 def pymol_check(args):
     if args.Pymol:
@@ -145,8 +157,41 @@ def position_extractor(position):
         except:
             pass
     return(pos)
-           
-def pymol_scripter(analyte, path, df, neg, struct, rotation, framework, hide):
+
+def custom_files(custom, files):
+    column = 0
+    custom_list = []
+    for i in custom:
+        if column==0:
+            current = [i]
+            column=1
+        elif column==1:
+            current.append(i)
+            custom_list.append(current)
+            column= 0
+    custom_list.sort(key=lambda x:x[0])
+    custom_files = []
+    tops = []
+    for i in custom_list:
+        custom_file = False
+        top = False
+        for file in files:
+            if i[0] == file.split('/')[-1].split('.')[0]:
+                custom_file = file
+                try:
+                    top = int(i[1])
+                except:
+                    print("Non correct input of -c. Input should be each analyte followed by desired number of top residues. For example -c MO176_156 5 MO176_301 2")
+                    sys.exit(1)
+        if custom_file and top and len(custom)%2==0:
+            custom_files.append(custom_file)
+            tops.append(top)
+        else:
+            print("Non correct input of -c. Input should be each analyte followed by desired number of top residues. For example -c MO176_156 5 MO176_301 2")
+            sys.exit(1)
+    return(custom_files, tops)
+
+def pymol_scripter(analyte, path, df, neg, struct, rotation, framework, hide, top):
     #locate pdb and pse files. pse file have priority over pdb.
     pdb_file = glob.glob("data/pymol/" + '*.pdb')
     pse_file = glob.glob("data/pymol/" + '*.pse')
@@ -162,6 +207,8 @@ def pymol_scripter(analyte, path, df, neg, struct, rotation, framework, hide):
     #initiate lines for pymol script
     lines = ['#!/usr/bin/env python3',
             '# -*- coding: utf-8 -*',
+            '',
+            '#Initialize PyMol and load file',
             'from pymol import cmd',
             'cmd.delete("all")',
             f'cmd.load("{file}")',
@@ -169,6 +216,8 @@ def pymol_scripter(analyte, path, df, neg, struct, rotation, framework, hide):
             'cmd.refresh()']
 
     #define frameworks and regions to hide
+    lines.append('')
+    lines.append("#Regions to show and hide, including structural residues")
     for region in framework:
         lines.append(f'cmd.color("0xFFFFFF", "{region}")')
     if hide:
@@ -180,37 +229,70 @@ def pymol_scripter(analyte, path, df, neg, struct, rotation, framework, hide):
             residue = position_extractor(residue)
             lines.append(f'cmd.color("0x835A00", "resi {residue}")')
     
-    #color top 3 residues according to binding change
+    #color top residues according to binding change
+    lines.append('')
+    lines.append("#Top residues")
+    
     df = df.loc[df.index!=neg]
-    df_top =df.nsmallest(3, "log change")
+    """
+    df_top =df.nsmallest(top, "log change")
     for pos, col in zip(df_top.index, df_top["rgba_colors"]):   
         pos = position_extractor(pos)
         hex_col = matplotlib.colors.to_hex(col)
         hex_col = "0x" + hex_col[1:]
-        lines.append(f'cmd.color("{hex_col}", "resi {pos}")')   
+        lines.append(f'cmd.color("{hex_col}", "resi {pos}")') """  
+    df = df.sort_values(by='log change')
+    count = 0
+    for pos, col in zip(df.index, df["rgba_colors"]):
+        pos = position_extractor(pos)
+        hex_col = matplotlib.colors.to_hex(col)
+        hex_col = "0x" + hex_col[1:]
+        if count < top:
+            lines.append(f'cmd.color("{hex_col}", "resi {pos}")')
+        count += 1
 
     #rotation and save
+    lines.append('')
+    lines.append("#Refresh and save .png files from two angles")
     if rotation:
         rotation_init = f'{rotation}'
         lines.append(f'cmd.rotate("y", "{rotation_init}")')
     lines.append('cmd.refresh()')
-    lines.append(f'cmd.png("{analyte}_structure_1.png", "30cm", "30cm", dpi=300, ray=1)')
+    lines.append(f'cmd.png("{analyte}_top={top}_structure_1.png", "30cm", "30cm", dpi=300, ray=1)')
     lines.append('cmd.turn("y", 180)')
     lines.append('cmd.refresh()')
-    lines.append(f'cmd.png("{analyte}_structure_2.png", "30cm", "30cm", dpi=300, ray=1)')
+    lines.append(f'cmd.png("{analyte}_top={top}_structure_2.png", "30cm", "30cm", dpi=300, ray=1)')
 
-    with open(f'{path}{analyte}_{model}.py', 'w') as f:
+    #All residues
+    lines.append('')
+    lines.append("#All residues with corresponding color")
+    for pos, col in zip(df.index, df["rgba_colors"]):
+        pos = position_extractor(pos)
+        hex_col = matplotlib.colors.to_hex(col)
+        hex_col = "0x" + hex_col[1:]
+        if pos != "":
+            lines.append(f'#cmd.color("{hex_col}", "resi {pos}")')
+
+    with open(f'{path}{analyte}_{model}_top={top}.py', 'w') as f:
         for l in lines:
             f.write(l + "\n")
 
-def analysis_iterator(files, plot_parameter, barplot_path, pymol_path, plot_value_path, args):
+def analysis_iterator(files, plot_parameter, barplot_path, pymol_path, plot_value_path, args, tops):
     positive = args.Positive
     negative = args.Negative
     order_param = 'file'
     print('Determining the epitope for the following analytes:')
     files.sort()
+    tab_width = 7
     for file in files:
-        print(file.split('/')[-1].split('.')[0])
+        name = file.split('/')[-1].split('.')[0]
+        if len(name) > tab_width:
+            tab_width = len(name)
+    tab_width = tab_width + 3
+    print('Analyte', " "*(tab_width-7), "No of residues for PyMol (if used)")
+    for i in range(len(files)):
+        name = files[i].split('/')[-1].split('.')[0]
+        print(name, " "*(tab_width-len(name)), tops[i])
     print('\nMaking individual plots for each analyte')
     pymol_check(args)
     for i in tqdm(range(len(files))):
@@ -220,7 +302,7 @@ def analysis_iterator(files, plot_parameter, barplot_path, pymol_path, plot_valu
         df.to_csv(plot_value_path + analyte + '.csv')
         df = log_plot(df, plot_parameter, analyte, positive, order_param, barplot_path)
         if args.Pymol:
-            pymol_scripter(analyte, pymol_path, df, negative, args.Structural, args.Rotation, args.Framework, args.Hide)
+            pymol_scripter(analyte, pymol_path, df, negative, args.Structural, args.Rotation, args.Framework, args.Hide, tops[i])
         df = heatmap_transform(df, analyte)
         if 'df_heatmap' not in locals():
             df_heatmap = df
@@ -241,14 +323,13 @@ def plot_heatmap(df, heatmap_path):
 def main(args):
     input_dir = args.Input
     print(f'\nInput directory: {input_dir}')
-    files = make_filelist(input_dir)
-    
+    files, tops = make_filelist(input_dir, args)
     plot_parameter = 'gated mean FL3-A div mean FL1-A one-by-one'
     epitope_plot_path, pymol_path, plot_value_path = make_output()
     
     print('\n____________________________\nRunning')
     
-    df_heatmap = analysis_iterator(files, plot_parameter, epitope_plot_path, pymol_path, plot_value_path, args)
+    df_heatmap = analysis_iterator(files, plot_parameter, epitope_plot_path, pymol_path, plot_value_path, args, tops)
     print('\nMaking heatmap of all analytes')
     plot_heatmap(df_heatmap, epitope_plot_path)
        
@@ -270,5 +351,7 @@ if __name__ == '__main__':
     parser.add_argument("-hi", "--Hide", nargs='+', help="Regions to hide for pymol script")
     parser.add_argument("-r", "--Rotation", help="Pymol initial rotation on y axis")
     parser.add_argument("-s", "--Structural", nargs='+', help="Structural positions")
+    parser.add_argument("-t", "--Top", help="Number of positions to highlight in pymol script. Default is 3", nargs='?', const=3, type=int, default=3)
+    parser.add_argument("-c", "--Custom", nargs='+', help="Analytes to include and number of positions to highlight in pymol script per analyte. This feature overrides --Top.")
     args = parser.parse_args()  
     main(args)
